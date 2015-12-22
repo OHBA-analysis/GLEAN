62023db05be87d49bf11e286ff74813b93d0ba0a
Adam Baker <adampbaker@users.noreply.github.com>
2015-12-22 18:06:53 +0000

    add connectivity profile results option

--- glean_connectivityprofile.m
+++ glean_connectivityprofile.m
@@ -1,34 +1,63 @@
-function P = glean_connectivityprofile(hmm)
+function GLEAN = glean_connectivityprofile(GLEAN,settings)
 % Computes the "connectivity profile" P for each HMM state. 
 % The connectivity profile is defined for each state as the Euclidean 
-% difference between a particular voxel's covariance with all other voxels
+% distance between a particular voxel's covariance with all other voxels
 % within the state versus outside of the state.
 %
-% P = GLEAN_CONNECTIVITYPROFILE(hmm)
+% GLEAN = GLEAN_CONNECTIVITYPROFILE(GLEAN,SETTINGS)
 %
 % REQUIRED INPUTS:
-%   hmm     - HMM structure inferred by glean_infer_hmm()
-% 
-% OUTPUTS:
-%   P       - [channels x states] connectivity profiles
+%   GLEAN     - An existing GLEAN analysis
+%   settings  - Structure with the following fields:
+%                 .format    - Format to save maps as
+%                                permitted: 'mat','nii'
+%                                default: 'mat'
 %
 % Adam Baker 2015
 
-Nchannels = size(hmm.state(1).Cov,1);
+res = 'connectivity_profile';
 
-
-C = zeros(Nchannels,Nchannels,hmm.K);
-
-for k = 1:hmm.K
-    C(:,:,k) = corrcov(hmm.state(k).Cov);
+if numel(GLEAN.envelope.settings.freqbands) > 1
+    error('Connectivity profile is not yet supported for multiband data');
 end
 
-P = zeros(Nchannels,hmm.K);
+% Remove existing results:
+if isfield(GLEAN.results,res)
+    GLEAN.results = rmfield(GLEAN.results,res);
+end
+
+results = struct;
+
+% Set up file names
+results_dir = fullfile(GLEAN.results.dir,res); % make this an option
+
+group_maps   = fullfile(results_dir,strcat('group_',res));
+
+% Duplicate maps across each frequency band:
+group_maps = strcat(group_maps,settings.format);
+results.groupmaps = group_maps;
+
+
+% Load model
+model = load(GLEAN.model.model);
+
+
+
+
+Nchannels = size(model.hmm.state(1).Cov,1);
+
+C = zeros(Nchannels,Nchannels,model.hmm.K);
+
+for k = 1:model.hmm.K
+    C(:,:,k) = corrcov(model.hmm.state(k).Cov);
+end
+
+P = zeros(Nchannels,model.hmm.K);
 
 channels = 1:Nchannels;
-states = 1:hmm.K;
+states = 1:model.hmm.K;
 
-C(repmat(logical(eye(Nchannels)),[1,1,hmm.K])) = nan;
+C(repmat(logical(eye(Nchannels)),[1,1,model.hmm.K])) = nan;
 
 for channel = channels
     
@@ -44,12 +73,21 @@ for channel = channels
 end
 
 
-% Nchannels = size(hmm.state(1).Cov,1);
-% 
-% 
-% P = zeros(Nchannels,hmm.K);
-% 
-% for k = 1:hmm.K
-%     C = corrcov(hmm.state(k).Cov);
-%     P(:,k) = nanmedian(diag(diag(nan(size(C)))) + C);
-% end
+% Save the group maps
+map = P;
+switch settings.format
+    case 'mat'
+        save(results.groupmaps,'map');
+    case 'nii'
+        map = parcellation2map(map,GLEAN.subspace.settings.parcellation.file,GLEAN.envelope.settings.mask);
+        writenii(map,results.groupmaps,GLEAN.envelope.settings.mask);
+end
+
+
+% Append results and settings to GLEAN:
+GLEAN.results.(res) = results;
+GLEAN.results.(res).settings  = settings;
+
+% Save updated GLEAN:
+save(GLEAN.name,'GLEAN');
+
