49315f5981bf033ff087bc62cf6697b76a0c73ce
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2016-03-09 09:26:24 +0000

    Fix indexing for trialwise data

--- glean_cov.m
+++ glean_cov.m
@@ -3,8 +3,8 @@ function [C,M] = glean_cov(D)
 %
 % [C,M] = GLEAN_COV(GLEAN)
 %
-% Computes the covariance matrix without encountering memory issues. This 
-% allows the covariance matrix to be computed for large data matrices 
+% Computes the covariance matrix without encountering memory issues. This
+% allows the covariance matrix to be computed for large data matrices
 % without running out of memory.
 %
 % This function can also compute the (channels x channels x trials) covariance
@@ -34,16 +34,15 @@ else
     samples2use = true(nsamples,1);
 end
 
-
 M = zeros(nchans,ntrials,nfreqs);
 C = zeros(nchans,nchans,ntrials,nfreqs);
-   
+
 for f = 1:nfreqs
-             
+
     for trl = 1:ntrials
-            
+
         nsamples = sum(samples2use(:,trl));
-        
+
         % Compute means
         chan_blks = memblocks([nchans,sum(samples2use(:,trl))],1);
         for i = 1:size(chan_blks,1)
@@ -57,27 +56,27 @@ for f = 1:nfreqs
             Dblk = squeeze(Dblk);
             M(chan_blks(i,1):chan_blks(i,2),trl,f) = mean(Dblk,2);
         end
-        
+
         % Compute covariance
         smpl_blks = memblocks([nchans,nsamples],2);
         for i = 1:size(smpl_blks,1)
-            
+
             samples2use_blk = find(samples2use(:,trl));
             samples2use_blk = samples2use_blk(smpl_blks(i,1):smpl_blks(i,2));
-            
+
             if isa(D,'meeg') && isequal(D.transformtype,'TF')
                 Dblk = squeeze(D(:,f,:,trl));
                 Dblk = Dblk(:,samples2use_blk(:,trl),:);
             else
                 Dblk = D(:,:,trl);
-                Dblk = Dblk(:,samples2use_blk(:,trl),:);
+                Dblk = Dblk(:,samples2use_blk,:);
             end
-            
+
             Dblk = bsxfun(@minus,Dblk,M(:,trl,f));
             C(:,:,trl,f) = C(:,:,trl,f) + Dblk*Dblk';
         end
         C(:,:,trl,f) = C(:,:,trl,f)./(nsamples-1);
-        
+
     end
 end
 
