980a7f9d315d58268ea356732ba2ccdfa5acf255
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-02-09 15:42:55 +0000

    Removed external libraries, updated startup script

--- glean_startup.m
+++ glean_startup.m
@@ -1,9 +1,9 @@
 here = fileparts(mfilename('fullpath'));
 
 % Add OHBA shared libraries
-if ~exist(fullfile(here,'..','osl-external2'))
-    fprintf(2,'Could not find ''%s''\n',fullfile(osldir,'osl-external'));
-    error('osl-external is missing. Clone https://github.com/OHBA-analysis/osl-external into the same directory as osl2');
+if ~exist(fullfile(here,'..','osl-external'))
+    fprintf(2,'Could not find ''%s''\n',fullfile(here,'..','osl-external'));
+    error('osl-external is missing. Clone https://github.com/OHBA-analysis/osl-external into the same directory as GLEAN');
 end
 
 addpath(fullfile(here,'..','osl-external'));
